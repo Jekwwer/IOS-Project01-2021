faae82315e568620ce32025511d8b5e43ffa5061
Evgenii Shiliaev <xshili00@stud.fit.vutbr.cz>
2021-03-28 22:35:09 +0200

    Added 'print_help' function

---
+++ tradelog.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export POSIXLY_CORRECT=yes
+export LC_NUMERIC=en_US.UTF-8
+
+function print_help() {
+    echo "help" #TODO
+}
+
+while getopts :ha:b:t:w: o
+do  case "$o" in
+      h)  print_help
+    esac
+done
