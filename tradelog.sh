73a55cfba1bbda45b7306844cd7261471dd95d3f
Evgenii Shiliaev <xshili00@stud.fit.vutbr.cz>
2021-03-31 13:36:04 +0200

    Remade INPUT PROCESSING and made some input changes in COMMAND PROCESSING

--- tradelog.sh
+++ tradelog.sh
@@ -68,9 +68,9 @@ function profit() {
 }
 
 function pos() {
-  arr=($(list_tick "-"))
-  for tick in ${arr[*]}; do
-    awk -v key=$tick -F';' '
+  TICKERS_ARRAY=($(list_tick "-"))
+  for tick in ${TICKERS_ARRAY[*]}; do
+    printf "$INPUT\n" | awk -v key=$tick -F';' '
     BEGIN {num = 0}
     {if ($2 == key && $3 == "buy")
         num = num + $6
@@ -78,19 +78,19 @@ function pos() {
         num = num - $6
     if ($2 == key)
         last = $4}
-    END {printf("%-10s:%12.2f\n", key, last*num)}' $1
+    END {printf("%-10s:%12.2f\n", key, last*num)}'
   done
 }
 
 # Function, that prints last price of a stock of each ticket
 function last_price() {
-  arr=($(list_tick "-"))
-  for tick in ${arr[*]}; do
-    awk -v key=$tick -F';' '
+  TICKERS_ARRAY=($(list_tick "-"))
+  printf "$INPUT\n" | for tick in ${TICKERS_ARRAY[*]}; do
+    printf "$INPUT\n" | awk -v key=$tick -F';' '
     BEGIN {last=0}
     {if ($2 == key)
         last = $4}
-    END {printf("%-10s:%8.2f\n", key, last)}' $1
+    END {printf("%-10s:%8.2f\n", key, last)}'
   done
 }
 
@@ -101,9 +101,11 @@ while getopts :ha:b:t:w: o; do
   h)
     print_help
     ;;
-  a) AFTER_DATE="$OPTARG"
+  a)
+    AFTER_DATE="$OPTARG"
     ;;
-  b) BEFORE_DETE="$OPTARG"
+  b)
+    BEFORE_DETE="$OPTARG"
     ;;
   t)
     TICKERS="$TICKERS $OPTARG"
@@ -129,44 +131,48 @@ for i in $*; do
   elif [ $1 == "last-price" ]; then
     LAST=1
   elif [ $1 == "--help" ]; then
-      print_help
+    print_help
   else
     LOG_FILE=$1
-#    if [[ "$LOG_FILE" == *".gz"* ]]; then
-#      gzip -dk "$LOG_FILE" #TODO delete 'k'
-#    fi
   fi
   shift
 done
 
+#INPUT PROCESSING
+if [ "$LOG_FILE" != "" ]; then
+  if [[ "$LOG_FILE" == *".gz"* ]]; then
+    INPUT=`gzip -dc "$LOG_FILE"`
+  else
+    INPUT=`cat "$LOG_FILE"`
+  fi
+else
+  INPUT=`cat`
+fi
+
 # COMMAND PROCESSING
 if [[ "$TICKERS" != "" ]]; then
   if [[ $TICK -eq 1 ]]; then
-    print_by_tickers $LOG_FILE | list_tick $STDINPUT
+    printf "$INPUT\n" | print_by_tickers | list_tick
   elif [[ $PROF -eq 1 ]]; then
-    print_by_tickers $LOG_FILE | profit $STDINPUT
+    printf "$INPUT\n" | print_by_tickers | profit
   elif [ $LAST -eq 1 ]; then
-    print_by_tickers $LOG_FILE | last_price $LOG_FILE
+    printf "$INPUT\n" | print_by_tickers | last_price
   elif [ $POS -eq 1 ]; then
-    print_by_tickers $LOG_FILE | pos $LOG_FILE | sort -k2nr -t:
+    printf "$INPUT\n" | print_by_tickers | pos | sort -k2nr -t:
   else
-    if [[ "$LOG_FILE" == *".gz"* ]]; then
-      gzip -dc "$LOG_FILE" | print_by_tickers #TODO remade .gz processing
-    fi
+    printf "$INPUT\n" | print_by_tickers
   fi
 else
   if [[ $TICK -eq 1 ]]; then
-    list_tick $LOG_FILE
+    printf "$INPUT\n" | list_tick
   elif [[ $PROF -eq 1 ]]; then
-    profit $LOG_FILE
+    printf "$INPUT\n" | profit
   elif [ $LAST -eq 1 ]; then
-    cat $LOG_FILE | last_price $LOG_FILE
-    elif [ $POS -eq 1 ]; then
-    cat $LOG_FILE | pos $LOG_FILE | sort -k2nr -t:
+    printf "$INPUT\n" | last_price
+  elif [ $POS -eq 1 ]; then
+    printf "$INPUT\n" | pos | sort -k2nr -t:
   else
-    while read A; do
-      echo $A
-    done
+    printf "$INPUT\n"
   fi
 fi
 
