3a51e952206b8ce72a1c82e21803eba06c23f7b5
Evgenii Shiliaev <xshili00@stud.fit.vutbr.cz>
2021-04-01 23:32:03 +0200

    Added test #2

--- tests.sh
+++ tests.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+function test() {
+  if [[ "$result" == "$expected" ]]; then
+    echo "Test passed!"
+  else
+    echo "Test failed!"
+    exit
+  fi
+}
+
 echo "Test #1"
   result=$(cat "stock-2.log" | head -n 5 | ./tradelog.sh)
   expected=$(cat "Control_Tests_Outputs/test01")
+  test
 
-if [[ "$result" == "$expected" ]]; then
-  echo "Test passed!"
-else
-  echo "Test failed!"
-  exit 1
-fi
+echo "Test #2"
+  result=$(./tradelog.sh -t TSLA -t V stock-2.log)
+  expected=$(cat "Control_Tests_Outputs/test02")
+  test
